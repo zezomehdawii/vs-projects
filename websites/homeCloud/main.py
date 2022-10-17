8f536c0769ce4a7553accb78ad6ecd86dfe7af33
zezomehdawii <zezomehdawi@gmail.com>
2022-10-17 16:02:29 +0300

    .....

--- websites/homeCloud/main.py
+++ websites/homeCloud/main.py
@@ -7,7 +7,7 @@ from wtforms.validators import InputRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
-app.config['UPLOAD_FOLDER'] = 'static/files'
+app.config['UPLOAD_FOLDER'] = 'files'
 
 class UploadFileForm(FlaskForm):
     file = FileField("File", validators=[InputRequired()])
@@ -21,7 +21,25 @@ def home():
         file = form.file.data # First grab the file
         file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
         return "File has been uploaded."
-    return render_template('index.html', form=form)
+    return render_template('index.html', form=form) 
+
+# @app.route('/files')
+# def get_files():
+#     folderPath = r"D:\\vs projects\\websites\\homeCloud\\files"
+#     files = os.listdir(folderPath)
+#     print (files)
+#     return render_template('files.html', files=files)
+path = r"d:\\vs projects\\websites\\homeCloud\\files" 
+
+@app.route('/files', defaults={'req_path': ''})
+@app.route('/files:\\vs projects\\websites\\homeCloud\\files>')
+def dir_listing(req_path):
+    global RENDER_OUTPUT_DIR
+    BASE_DIR = RENDER_OUTPUT_DIR
+    abs_path = os.path.join(BASE_DIR, req_path)
+    files = os.listdir(abs_path)
+    return render_template('files.html', files=files)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
